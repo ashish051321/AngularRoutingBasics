804b2b6e8dc2ddb358ea2d3604c27543afcf0479
ashish mishra <ashish.25jl@gmail.com>
2018-03-06 14:28:14 -0500

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  
+  constructor(private ren: Renderer2, private el: ElementRef){
+    
+  }
+  ngOnInit(){
+    this.ren.setStyle(this.el.nativeElement.querySelector(".mainNavigation"),"visibility","hidden");
+    this.ren.setStyle(this.el.nativeElement.querySelector(".navhook"),"visibility","visible");
+  }
+
+  adjustNav(){
+    this.ren.setStyle(this.el.nativeElement.querySelector(".mainNavigation"),"visibility","hidden");
+    this.ren.setStyle(this.el.nativeElement.querySelector(".navhook"),"visibility","visible");
+   
+    // this.ren.setStyle
+  }
+  bringOnNav(){
+    this.ren.setStyle(this.el.nativeElement.querySelector(".mainNavigation"),"visibility","visible");
+    this.ren.setStyle(this.el.nativeElement.querySelector(".navhook"),"visibility","hidden");
+  }
 }
