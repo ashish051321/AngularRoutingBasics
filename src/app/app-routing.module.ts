7d3d355be45a203265ea771f94b910ae8e4869a0
ashish mishra <ashish.25jl@gmail.com>
2018-03-12 17:51:13 +0530

    done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
 const routes: Routes = [
   { path: '', redirectTo: "welcome", pathMatch: "full" },
   { path: 'welcome', component: WelcomeComponent },
-  { path: "home", loadChildren: './managequestions/managequestions.module#ManagequestionsModule' },
+  { path: "questions", loadChildren: './managequestions/managequestions.module#ManagequestionsModule' },
   { path: '**', component: NotfoundComponent }, //always last
 ];
 
--- src/app/app2/app2.component.ts
+++ src/app/app2/app2.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'myAngularApp',
@@ -7,14 +8,14 @@ import { Component, OnInit, ElementRef } from '@angular/core';
 })
 export class App2Component implements OnInit {
 
-  constructor(private el: ElementRef) { }
-  
+  constructor(private el: ElementRef, private router: Router) { }
+
   currentTitle = "Home";
 
   ngOnInit() {
   }
 
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+  /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
   openNav() {
     this.el.nativeElement.querySelector("#mySidenav").style.width = "250px";
     // this.el.nativeElement.querySelector("#main").style.marginLeft = "250px";
@@ -22,12 +23,22 @@ export class App2Component implements OnInit {
   }
 
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-closeNav() {
-  this.el.nativeElement.querySelector("#mySidenav").style.width = "0";
-  // this.el.nativeElement.querySelector("#main").style.marginLeft = "0";
-  // document.body.style.backgroundColor = "white";
-  return false;
-}
+  /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+  closeNav() {
+    this.el.nativeElement.querySelector("#mySidenav").style.width = "0";
+    // this.el.nativeElement.querySelector("#main").style.marginLeft = "0";
+    // document.body.style.backgroundColor = "white";
+
+  }
+
+
+  navigateTo() {
+    this.el.nativeElement.querySelector("#mySidenav").style.width = "0";
+    this.router.navigate(['questions']);
+  }
+
+  navigateToPart2(){
+    this.el.nativeElement.querySelector("#mySidenav").style.width = "0";
+  }
 
 }
